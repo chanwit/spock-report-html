66a8e20b43d925652dd6b707f5da312a5301069a
Chanwit Kaewkasi <chanwit@gmail.com>
2012-03-09 21:29:58 +0700

    fixed #4 - add an embedded style sheet

--- src/main/groovy/custom/ReportExtension.groovy
+++ src/main/groovy/custom/ReportExtension.groovy
@@ -25,9 +25,31 @@ class ReportExtension implements IGlobalExtension {
                 buffer << "<html>\n"
                 buffer << """\
 <head>
+  <meta charset="utf-8" />
   <title>
     Specification ${specInfo.description}
   </title>
+  <style>
+    table.whereBlock {
+        background-color:#ffffff;
+        border:1px solid #c3c3c3;
+        border-collapse:collapse;
+    }
+
+    table.whereBlock th {
+        background-color:#e3e3e3;
+        border:1px solid #c3c3c3;
+        padding:3px;
+        vertical-align:top;
+    }
+
+    table.whereBlock td {
+        border:1px solid #c3c3c3;
+        padding:3px;
+        vertical-align:top;
+    }
+
+  </style>
 </head>
 
 """
@@ -37,15 +59,29 @@ class ReportExtension implements IGlobalExtension {
 
             @Override
             void beforeFeature(FeatureInfo feature) {
-                buffer << "<h2>Feature: ${feature.name}</h2>"
+                buffer << "<h2>Feature: ${feature.name}</h2>\n"
                 iterBody = new StringBuilder()
                 dataVars = feature.dataVariables
-                iterBody << "<div>${dataVars}</div>"
+                iterBody << "<table class=\"whereBlock\">\n"
+                iterBody << "<tbody>\n"
+                iterBody << "<tr>\n"
+                dataVars.each {
+                    iterBody << "<th>${it}</th>\n"
+                }
+                iterBody << "</tr>\n"
             }
 
             @Override
             void beforeIteration(IterationInfo iteration) {
-                iterBody << "<div>${iteration.dataValues}</div>"
+                iterBody << "<tr>\n"
+                iteration.dataValues.each {
+                    if(it instanceof String) {
+                        iterBody << "<td>\"${it}\"</td>\n"
+                    } else {
+                        iterBody << "<td>${it}</td>\n"
+                    }
+                }
+                iterBody << "</tr>\n"
             }
 
             @Override
@@ -53,18 +89,20 @@ class ReportExtension implements IGlobalExtension {
                 for (block in feature.blocks) {
                     def kindText = ""
                     switch(block.kind) {
-                        case BlockKind.SETUP:  kindText = "Given "  ; break
-                        case BlockKind.WHEN:   kindText = "When "   ; break
-                        case BlockKind.THEN:   kindText = "Then "   ; break
-                        case BlockKind.EXPECT: kindText = "Expect " ; break
-                        case BlockKind.WHERE:  kindText = "Where "  ; break
+                        case BlockKind.SETUP:  kindText = "Given " ; break
+                        case BlockKind.WHEN:   kindText = "When "  ; break
+                        case BlockKind.THEN:   kindText = "Then "  ; break
+                        case BlockKind.EXPECT: kindText = "Expect "; break
+                        case BlockKind.WHERE:  kindText = "Where " ; break
                     }
                     buffer <<  "<div class=\"block-${block.kind}\">${kindText}"
                     for (text in block.texts) {
-                        buffer << "<span>${text}</span>"
+                        buffer << "<span>${text}</span>\n"
                     }
-                    buffer << "</div>"
+                    buffer << "</div>\n"
                 }
+                iterBody << "</tbody>\n"
+                iterBody << "</table>\n"
                 buffer << iterBody.toString()
             }
 
@@ -81,7 +119,11 @@ class ReportExtension implements IGlobalExtension {
 
             @Override
             void error(ErrorInfo error) {
-                iterBody << "<div><pre>${error.error.message}</pre></div>"
+                iterBody << """\
+            <tr>
+                <td colspan=\"${dataVars.size()}\"><pre>${error.error.message}</pre></td>
+            </tr>
+"""
             }
 
         })
